041d2d2f1963983a0316077cf14c05c6f9d1a5d6
DongjaeLEE <dongjae0107@gmail.com>
2025-11-20 16:19:28 +0900

    Update download link

--- scripts/download_parkinglot.sh
+++ scripts/download_parkinglot.sh
@@ -4,32 +4,32 @@ mkdir -p data/parkinglot
 cd data/parkinglot
 
 # 01.zip
-gdown https://drive.google.com/uc?id=1-O_Mle53luhaEEFh5YWWkThy29jzwjf_
+gdown https://drive.google.com/uc?id=1jZJQQKLAFIvPAIda4a0LsfNrUVQsqVBK
 unzip -q 01.zip -d 01
 rm 01.zip
 
 # 02.zip
-gdown https://drive.google.com/uc?id=19sjinWpx5AvxUtRkprIVdaBjod8gkoyW
+gdown https://drive.google.com/uc?id=1WpqRhpLyCIUKhd_aFwm687POBhzHiXM6
 unzip -q 02.zip -d 02
 rm 02.zip
 
 # # 03.zip
-# gdown https://drive.google.com/uc?id=1KrSoxT2mARoEIR08her0VUmdvBOjm779
+# gdown https://drive.google.com/uc?id=1LixqhVSpaU9svrY4nzKk4gCU1hEd3bnC
 # unzip -q 03.zip -d 03
 # rm 03.zip
 
 # # 04.zip
-# gdown https://drive.google.com/uc?id=1_aLHcMdjuNir47UINDffN2_S30xvn0Gc
+# gdown https://drive.google.com/uc?id=1A5C5Iv79HHAapmoi6gIA5SfF0d7HJEvd
 # unzip 04.zip
 # rm 04.zip
 
 # # 05.zip
-# gdown https://drive.google.com/uc?id=1Pc5hHR0hxbCtVGOXQxPIigpqXh2ASFmb
+# gdown https://drive.google.com/uc?id=1pO-a_NgQS9-yXpH6ro9CBapSk1RnHVBO
 # unzip 05.zip
 # rm 05.zip
 
 # # 06.zip
-# gdown https://drive.google.com/uc?id=1hm8fGQPAAmviuFmT1WZl73qEjAqLZ6xX
+# gdown https://drive.google.com/uc?id=1LMqgBoPnb9C9cY35qy-dlsiVKKNSb0xv
 # unzip 06.zip
 # rm 06.zip
 
